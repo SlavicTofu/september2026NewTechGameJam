ca77e9af89c3551d9632d521700c9402aff9298d
SlavicTofu <80066554+SlavicTofu@users.noreply.github.com>
2026-09-19 16:36:16 -0400

    Added Enemy script, removed Entity Scene
    I realized that since Enemy and Entity are abstract classes, they should not have scenes associated.
    
    I also applied an underscore to the delta in Entity's _process function as it is not used (because it is a virtual function)

---
+++ entity.gd
@@ -0,0 +1,53 @@
+class_name Entity
+extends CharacterBody2D
+
+@onready var sprite = $Sprite
+@onready var hurtbox = $Hurtbox
+
+@export var health = 5
+
+var _direction
+
+enum Direction {
+	NORTH,
+	NORTHEAST,
+	EAST,
+	SOUTHEAST,
+	SOUTH,
+	SOUTHWEST,
+	WEST,
+	NORTHWEST,
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+	
+
+func poll_direction() -> Direction:
+	var dx = velocity.x
+	var dy = velocity.y
+	return (
+		Direction.SOUTHWEST if (dx < 0 and dy > 0)
+		else Direction.SOUTHEAST if (dx > 0 and dy > 0)
+		else Direction.NORTHWEST if (dx < 0 and dy < 0)
+		else Direction.NORTHEAST if (dx > 0 and dy < 0)
+		else Direction.WEST if (dx < 0)
+		else Direction.EAST if (dx > 0)
+		else Direction.NORTH if (dy > 0)
+		else Direction.SOUTH if (dy < 0)
+		else _direction
+	)
+
+
+func _move() -> void:
+	pass # STUB: Virtual function.
+	
+	
+func _attack() -> void:
+	pass # STUB: Virtual function
